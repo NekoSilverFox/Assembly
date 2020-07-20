05639c7e6af8034ee39ed7111521fcaa88ba9c3c
foxthere <weidufox@gmail.com>
2020-07-20 13:54:26 +0300

    增加 26 - 将datasg段中的所有单词改为大写

---
+++ 26 - 将datasg段中的所有单词改为大写/26 - 将datasg段中的所有单词改为大写.asm
@@ -0,0 +1,43 @@
+; 26 - 将datasg段中的所有单词改为大写
+; =================================================================
+; 将datasg段中的所有单词改为大写
+; 	习惯：si - 数据从哪里来
+; 	习惯：di - 数据到哪里去
+; 	本测试属于P154，问题7.7
+; =================================================================
+
+assume cs:codesg, ds:datasg
+
+datasg segment
+				db 'ibm             '
+				db 'dec             '
+				db 'dos             '
+				db 'vax             '
+datasg ends
+
+;===================================
+
+codesg segment
+start:			mov ax, datasg
+				mov ds, ax
+
+				mov si, 0	; 行
+				mov cx, 4
+
+loopRow:		push cx
+				mov cx, 3
+				mov di, 0
+
+		loopCol:		mov al, ds:[si + di]
+						and al, 11011111B		; 注意！读取的是一个字母，所以是一字节！！用字节型寄存器！！！
+						mov ds:[si + di]
+						inc di
+						loopCol
+
+				pop cx
+				add  si, 16
+				loop loopRow
+
+codesg ends
+
+end start
