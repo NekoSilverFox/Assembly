dae7e33a8fad79b949c2e3bd7fd84264147719ed
foxthere <weidufox@gmail.com>
2020-09-17 18:13:35 +0300

    增加 301 - 在VS中创建64位汇编项目

---
+++ 301 - 在VS中创建64位汇编项目/asm_x64.asm
@@ -0,0 +1,13 @@
+; https://blog.csdn.net/Alisebeast/article/details/79875620 如何设置VS
+		.code			;代码段      ！64 位没有 .model 宏指令，无法设置内存模型和生成的代码风格！
+main proc				;main函数开始
+
+	mov rax, 120
+	add rax, 34
+	sub rax, 130
+
+	ret
+
+main endp				;main函数结束
+end
+					;masm x64 没有指定入口点的宏指令！
